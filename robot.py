0a551076d7486b5c65d3d4bd5e4283e59e4f8f31
Andresblz <57406530+Andresblz@users.noreply.github.com>
2024-05-13 15:27:21 +0200

    Add files via upload

--- robot.py
+++ robot.py
@@ -4,9 +4,10 @@ import math
 
 class Robot:
     # wheel_radius=??, reduction=??, encoder_res=??, between_wheels_dist=??
-    def __init__(self, port="/dev/ttyUSB0", speed=1000, wheel_radius=36, reduction=508.8, encoder_res=1, between_wheels_dist=235):
+    def __init__(self, port="/dev/tty.usbserial-DN04GLBY", speed=500, wheel_radius=36, reduction=508.8, encoder_res=1, between_wheels_dist=235):
         Robot.initPos = (0, 0)
-        Robot.initOrientation = 0.
+        Robot.initOrientation = 0
+        Robot.errorThres = 0.0001
 
         # Iniciar robot en modo `Safe`
         self.robot = Create2(port)
@@ -35,10 +36,10 @@ class Robot:
         current_distance = 0
         self.robot.drive_direct(self.speed, self.speed)
         while current_distance < distance:
-                prev_encoder = self.encoder_values
-                self.__update_all_sensors()
-                encoder_dif = (self.encoder_values[0] - prev_encoder[0], self.encoder_values[1] - prev_encoder[1])
-                current_distance += self.conversion_factor * encoder_dif[0]
+            prev_encoder = self.encoder_values
+            self.__update_all_sensors()
+            encoder_dif = (self.encoder_values[0] - prev_encoder[0], self.encoder_values[1] - prev_encoder[1])
+            current_distance += self.conversion_factor * encoder_dif[0]
         self.robot.drive_stop()	
 
 
@@ -49,19 +50,67 @@ class Robot:
         # self.x += distance * math.cos(math.radians(self.theta))
         # self.y += distance * math.sin(math.radians(self.theta))
 
+    def log(self, message):
+        with open("robot_log7.txt", 'a') as file:
+            file.write(message + '\n')
+
     def rotate(self, angle):
-        current_rotation = 0;
-        self.robot.drive_direct(self.speed, -self.speed)
+        counter = 0
+        # Multiplicador del counter para reducir velocidad
+        counterLim = 100
+        current_rotation = 0
         angle = angle * math.pi / 180.
-        print(angle)
-        while current_rotation < angle:
+        self.log("[INFO] Target angle: " + str(angle))
+        # se presupone que inicialmente el error es positivo
+        flag = True
+        error = angle
+        # girar al sentido agujas del reloj
+        self.robot.drive_direct(self.speed, -self.speed)
+        # comparamos el error hasta que sea menor que nuestro threshold
+        
+        while abs(error) >= self.errorThres:
+                #
+                # comparamos hacia que lado tiene que girar en funcion de su error
+                #
+                # tiene que girar al contrario agujas del reloj
+                if error > 0:
+                    # si anteriormente su error era negativo
+                    if not flag:
+                        counter += 1
+                        flag = True
+                        self.robot.drive_stop()
+                        self.robot.drive_direct(self.speed, -self.speed)
+
+                        if self.speed > 20 + counterLim:
+                            self.speed = self.speed - (counterLim)
+                        else: 
+                            self.speed = 20
+                        self.log("[INFO] Speed adjusted (Clockwise): " + str(self.speed))
+                # tiene que girar al sentido de agujas del reloj
+                elif error < 0:
+                    # si anteriormente su error era positivo
+                    if flag: 
+                        counter += 1                   
+                        flag = False
+                        self.robot.drive_stop()
+                        self.robot.drive_direct(-self.speed, self.speed)
+                        if self.speed > 20 + counterLim:
+                            self.speed = self.speed - (counterLim)
+                        else: 
+                            self.speed = 20
+                        self.log("[INFO] Speed adjusted (Counter-clockwise): " + str(self.speed))
+                
                 prev_encoder = self.encoder_values
                 self.__update_all_sensors()
                 encoder_dif = (self.encoder_values[0] - prev_encoder[0], self.encoder_values[1] - prev_encoder[1])
                 current_rotation -= 2 * encoder_dif[0] * self.conversion_factor / self.between_wheels_dist
-                print(f"{self.encoder_values[0]}, {prev_encoder[0]}, {self.encoder_values[1]}, {prev_encoder[1]}")
-                print(current_rotation)
+                #self.log("[INFO] Encoder values: " + str(self.encoder_values))
+                #self.log("[INFO] Current rotation: " + str(current_rotation))
+                
+                error = angle-current_rotation
+                self.log("[INFO] Error: " + str(error))
         self.robot.drive_stop()
+        self.log("[INFO] Rotation complete. Stopping robot.")
                 
         # Rotación del robot en base a un ángulo dado y actualización de la orientación
         # self.robot.drive_direct(-self.speed, self.speed)
@@ -99,7 +148,8 @@ robot = Robot()
 odometry_calculator = OdometryCalculator(robot)
 
 # Movimiento del robot y actualización de las distancias de los sensores
-robot.move(500)  # Por ejemplo, mueve 500 mm hacia adelante
+#robot.move(500)  # Por ejemplo, mueve 500 mm hacia adelante
+log_file = "robot_log.txt"
 robot.rotate(90)  # Por ejemplo, rota 90 grados a la izquierda
 # robot.update_distance_sensors([8, 12, 9])  # Supongamos que los sensores detectan estas distancias
 
